3ea6a7e649c2f6b39e05093bc25f76a4f2e9f3e2
weirdwolf962-dot <weirdwolf962@gmail.com>
2025-12-12 18:00:45 +0530

    Add files via upload

---
+++ Zephyr5.1-main/Zephyr5.1-main/types.ts
@@ -0,0 +1,12 @@
+export enum Role {
+  USER = 'user',
+  MODEL = 'model',
+}
+
+export interface Message {
+  role: Role;
+  text: string;
+  sources?: { title: string; uri: string }[];
+  image?: string;
+  agentName?: string;
+}
